c9b49acd6e21075adcc7eef6639cf12c8e6b5175
Joe Bae <lineanity@gmail.com>
2024-12-06 02:53:25 +0700

    testing farcaster frame

--- lib/config.ts
+++ lib/config.ts
@@ -1,17 +1,20 @@
 import { http, createConfig } from "wagmi";
 import { base, mainnet, optimism } from "wagmi/chains";
-import { metaMask, coinbaseWallet } from "wagmi/connectors";
+import { metaMask, coinbaseWallet, injected } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base, mainnet, optimism],
   ssr: true,
   connectors: [
     coinbaseWallet({
-        appName: "Four Lucky", 
-        appLogoUrl: "https://fourlucky.vercel.app/placeholder.webp",
-        preference: "all",
+      appName: "Four Lucky",
+      appLogoUrl: "https://fourlucky.vercel.app/placeholder.webp",
+      preference: "all",
     }),
     metaMask(),
+    injected({
+      target: "rainbow"
+    }),
   ],
   transports: {
     [base.id]: http(),
