22e3eed1828d54285b8fd6cddd207cd03ca27027
Joe Bae <lineanity@gmail.com>
2025-02-03 13:20:53 +0700

    minor fix

--- app/components/FourNumberPlay.tsx
+++ app/components/FourNumberPlay.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import sdk from "@farcaster/frame-sdk";
 import {
     BaseError,
@@ -25,31 +25,11 @@ import NumberSeven from '../number/Seven';
 import NumberEight from '../number/Eight';
 import NumberNine from '../number/Nine';
 import { base } from "wagmi/chains";
-import Disclaimer from "./Disclaimer";
 
 const FourNumberContract = "0xe25C578b2F087381B713F482Bf3AA954cff2125e" as `0x${string}`;
 
 const FourNumberPlay = () => {
-    const [isSDKLoaded, setIsSDKLoaded] = useState(false);
     const [userNumbers, setUserNumbers] = useState<number[]>([]);
-
-    const audioRef = useRef<HTMLAudioElement>(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [showOverlay, setShowOverlay] = useState(true);
-
-    // Toggle audio play and pause
-    const handleAudioPlayPause = () => {
-        const audio = audioRef.current;
-        if (!audio) return;
-
-        if (isPlaying) {
-            audio.pause();
-        } else {
-            audio.play().catch((err) => console.error("Audio play error:", err));
-        }
-
-        setIsPlaying(!isPlaying);
-    };
 
     const { isConnected } = useAccount();
     const chainId = useChainId();
@@ -60,17 +40,6 @@ const FourNumberPlay = () => {
         useWaitForTransactionReceipt({
             hash,
         })
-
-    useEffect(() => {
-        const load = async () => {
-            sdk.actions.ready();
-        };
-
-        if (sdk && !isSDKLoaded) {
-            setIsSDKLoaded(true);
-            load();
-        }
-    }, [isSDKLoaded]);
 
     useEffect(() => {
         if (isConfirmed) {
@@ -114,40 +83,8 @@ const FourNumberPlay = () => {
 
     const endTime = data?.[1] ? Number(data[1]) * 1000 : null; // Convert seconds to milliseconds
 
-    const handleOpen = () => {
-        const audio = audioRef.current;
-
-        if (audio) {
-            audio.play().catch((err) => console.error("Audio play error:", err));
-            setIsPlaying(true);
-        }
-
-        setShowOverlay(false);
-    };
-
-    const handleClose = () => {
-        history.back();
-    };
-
-    if (!isSDKLoaded) {
-        return <></>;
-    }
-
     return (
         <div className="w-full flex flex-col sm:flex-row gap-4">
-            {/* Background Music */}
-            <audio
-                loop
-                ref={audioRef}
-                controls={false}
-                onContextMenu={(e) => e.preventDefault()}
-            >
-                <source
-                    src="/audio/clank-clank-clank.mp3"
-                    type="audio/mp3"
-                />
-                Your browser does not support the audio element.
-            </audio>
             {/* Selected Numbers */}
             <div className="basis-1/2 grid grid-cols-2 gap-4 bg-[#282828] rounded-2xl p-4">
                 {Array.from({ length: 4 }).map((_, index) => {
@@ -309,43 +246,6 @@ const FourNumberPlay = () => {
                 <div className="absolute top-0 left-0 w-40 h-40 bg-yellow-300 rounded-full blur-xl" />
                 <div className="absolute bottom-0 right-0 w-60 h-60 bg-purple-400 rounded-full blur-2xl" />
             </div>
-
-            {/* Play/Pause Button */}
-            <button
-                onClick={handleAudioPlayPause}
-                className="fixed bottom-4 right-4 p-2 rounded-full bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500"
-            >
-                {isPlaying ? (
-                    <svg width="35" height="35" fill="none" viewBox="-0.5 0 25 25" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 6.42a3 3 0 0 0-6 0v12a3 3 0 1 0 6 0zm10 0a3 3 0 1 0-6 0v12a3 3 0 1 0 6 0z" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
-                ) : (
-                    <svg width="35" height="35" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16.658 9.286c1.44.9 2.16 1.35 2.407 1.926a2 2 0 0 1 0 1.576c-.247.576-.967 1.026-2.407 1.926L9.896 18.94c-1.598.999-2.397 1.498-3.056 1.445a2 2 0 0 1-1.446-.801C5 19.053 5 18.111 5 16.226V7.774c0-1.885 0-2.827.394-3.358a2 2 0 0 1 1.446-.801c.66-.053 1.458.446 3.056 1.445z" stroke="#FFFFFF" strokeWidth="2" strokeLinejoin="round" />
-                    </svg>
-                )}
-            </button>
-
-            {showOverlay && (
-                <div className="absolute inset-0 mx-auto backdrop-blur-md flex flex-col items-center justify-center z-10">
-                    <Disclaimer>
-                        <div className="text-white flex flex-row gap-4 items-center justify-center">
-                            <button
-                                onClick={handleClose}
-                                className="w-full p-3 bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500 rounded-lg font-mono"
-                            >
-                                Close
-                            </button>
-                            <button
-                                onClick={handleOpen}
-                                className="w-full p-3 bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500 rounded-lg font-mono"
-                            >
-                                Open
-                            </button>
-                        </div>
-                    </Disclaimer>
-                </div>
-            )}
 
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,123 @@
+"use client"
+
+import { useEffect, useRef, useState } from "react";
 import FourNumberPlay from "./components/FourNumberPlay";
 import RoundStatsData from "./components/RoundStatsData";
 import UserStatsData from "./components/UserStatsData";
+import Disclaimer from "./components/Disclaimer";
+import sdk from "@farcaster/frame-sdk";
 
 
 export default function Home() {
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  useEffect(() => {
+    const load = async () => {
+      sdk.actions.ready();
+    };
+
+    if (sdk && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
+
+  // Toggle audio play and pause
+  const handleAudioPlayPause = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play().catch((err) => console.error("Audio play error:", err));
+    }
+
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleOpen = () => {
+    const audio = audioRef.current;
+
+    if (audio) {
+      audio.play().catch((err) => console.error("Audio play error:", err));
+      setIsPlaying(true);
+    }
+
+    setShowOverlay(false);
+  };
+
+  const handleClose = () => {
+    history.back();
+  };
+
+  if (!isSDKLoaded) {
+    return <></>;
+  }
 
   return (
     <div className="p-4">
-      <FourNumberPlay />
-      <RoundStatsData />
-      <UserStatsData />
+
+      {/* Background Music */}
+      <audio
+        loop
+        ref={audioRef}
+        controls={false}
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        <source
+          src="/audio/clank-clank-clank.mp3"
+          type="audio/mp3"
+        />
+        Your browser does not support the audio element.
+      </audio>
+
+      {/* Play/Pause Button */}
+      <button
+        onClick={handleAudioPlayPause}
+        className="fixed bottom-4 right-4 p-2 rounded-full bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500"
+      >
+        {isPlaying ? (
+          <svg width="35" height="35" fill="none" viewBox="-0.5 0 25 25" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 6.42a3 3 0 0 0-6 0v12a3 3 0 1 0 6 0zm10 0a3 3 0 1 0-6 0v12a3 3 0 1 0 6 0z" stroke="#FFFFFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        ) : (
+          <svg width="35" height="35" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.658 9.286c1.44.9 2.16 1.35 2.407 1.926a2 2 0 0 1 0 1.576c-.247.576-.967 1.026-2.407 1.926L9.896 18.94c-1.598.999-2.397 1.498-3.056 1.445a2 2 0 0 1-1.446-.801C5 19.053 5 18.111 5 16.226V7.774c0-1.885 0-2.827.394-3.358a2 2 0 0 1 1.446-.801c.66-.053 1.458.446 3.056 1.445z" stroke="#FFFFFF" strokeWidth="2" strokeLinejoin="round" />
+          </svg>
+        )}
+      </button>
+
+      {showOverlay ? (
+        <div className="absolute inset-0 mx-auto backdrop-blur-md flex flex-col items-center justify-center z-10">
+          <Disclaimer>
+            <div className="text-white flex flex-row gap-4 items-center justify-center">
+              <button
+                onClick={handleClose}
+                className="w-full p-3 bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500 rounded-lg font-mono"
+              >
+                Close
+              </button>
+              <button
+                onClick={handleOpen}
+                className="w-full p-3 bg-gradient-to-r from-orange-500 to-pink-800 hover:from-pink-800 hover:to-orange-500 rounded-lg font-mono"
+              >
+                Open
+              </button>
+            </div>
+          </Disclaimer>
+        </div>
+      ) : (
+        <>
+          <FourNumberPlay />
+          <RoundStatsData />
+          <UserStatsData />
+        </>
+      )}
     </div>
   );
 }
